a425034ce5eff8868b9deea0b09631dc7fa5a84d
khumphr <khumphr@gmail.com>
2016-07-25 07:49:41 -0700

    added bouncy and icy scripts

---
+++ STEM Challenge 2016/Assets/Scripts/BounceTileTrigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BounceTileTrigger : MonoBehaviour {
+
+	void OnTriggerEnter(Collider player)
+	{
+		player.GetComponent<SphereCollider> ().material.bounciness = 1;
+		player.GetComponent<SphereCollider> ().material.dynamicFriction = 0;
+		player.GetComponent<SphereCollider> ().material.staticFriction = 0;
+		player.GetComponent<SphereCollider> ().material.frictionCombine = PhysicMaterialCombine.Maximum;
+		player.GetComponent<SphereCollider> ().material.bounceCombine = PhysicMaterialCombine.Maximum;
+
+		player.GetComponent<Rigidbody> ().drag = 0;
+		player.GetComponent<Rigidbody> ().angularDrag = 0;
+	}
+
+	void OnTriggerExit(Collider player)
+	{
+		player.GetComponent<SphereCollider> ().material.bounciness = 0.2f;
+		player.GetComponent<SphereCollider> ().material.dynamicFriction = 0.6f;
+		player.GetComponent<SphereCollider> ().material.staticFriction = 0.6f;
+		player.GetComponent<SphereCollider> ().material.frictionCombine = PhysicMaterialCombine.Average;
+		player.GetComponent<SphereCollider> ().material.bounceCombine = PhysicMaterialCombine.Average;
+
+		player.GetComponent<Rigidbody> ().drag = 1;
+		player.GetComponent<Rigidbody> ().angularDrag = 0.05f;
+	}
+}
---
+++ STEM Challenge 2016/Assets/Scripts/IceTileTrigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IceTileTrigger : MonoBehaviour {
+
+	void OnTriggerEnter(Collider player)
+	{
+		player.GetComponent<SphereCollider> ().material.bounciness = 0;
+		player.GetComponent<SphereCollider> ().material.dynamicFriction = 0;
+		player.GetComponent<SphereCollider> ().material.staticFriction = 0;
+		player.GetComponent<SphereCollider> ().material.frictionCombine = PhysicMaterialCombine.Minimum;
+		player.GetComponent<SphereCollider> ().material.bounceCombine = PhysicMaterialCombine.Minimum;
+
+		player.GetComponent<Rigidbody> ().drag = 0;
+		player.GetComponent<Rigidbody> ().angularDrag = 0;
+	}
+
+	void OnTriggerExit(Collider player)
+	{
+		player.GetComponent<SphereCollider> ().material.bounciness = 0.2f;
+		player.GetComponent<SphereCollider> ().material.dynamicFriction = 0.6f;
+		player.GetComponent<SphereCollider> ().material.staticFriction = 0.6f;
+		player.GetComponent<SphereCollider> ().material.frictionCombine = PhysicMaterialCombine.Average;
+		player.GetComponent<SphereCollider> ().material.bounceCombine = PhysicMaterialCombine.Average;
+
+		player.GetComponent<Rigidbody> ().drag = 1;
+		player.GetComponent<Rigidbody> ().angularDrag = 0.05f;
+	}
+}
